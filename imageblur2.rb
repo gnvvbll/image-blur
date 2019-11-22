8381f52b058e7655a4e8019d997282cb802cef0b
gnvvbll <gnvvbll@gmail.com>
2019-11-22 12:14:22 -0500

    image blur 2

---
+++ imageblur2.rb
@@ -0,0 +1,58 @@
+class List
+    def initialize (array)
+        @array = array
+    end 
+
+    def find_ones 
+        points = []
+        @array.each_with_index do |row, row_pos|
+            #puts "\n", row, "\n"
+            row.each_with_index do |col, col_pos|
+                if col == 1
+                    #puts "found a one @ ", row_pos, " ", col_pos
+                    points.push [row_pos, col_pos]
+                end
+            end
+        end
+        return points
+    end
+
+    def blur
+        points = find_ones 
+        puts points
+        points.each do |point|
+            if point[1] != 0
+                @array[point[0]][point[1] - 1] = 1 #point [1] cannot be 0
+            end
+            if point [1] !=3
+            @array[point[0]][point[1] + 1] = 1 #point [1] cannot be 3
+            end
+            if point [0] != 0
+            @array[point[0] - 1][point[1]] = 1 #point [0] cannot be 0
+            end
+            if point [0] != 3
+            @array[point[0] + 1][point[1]] = 1 #point [0] cannot be 3
+            end
+        end
+        @array.each do |array|
+            puts array.join('')
+        end
+    end
+
+end
+
+list = List.new([
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0]
+        ])
+
+list.blur
+
+
+
+    
+
+
+
